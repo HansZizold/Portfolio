fb37d41801dffe1c3efca88a1d371ecc7336ec8c
IsaMarvin <marvinisra08@gmail.com>
2022-05-10 19:26:51 +0430

    Update script.js file

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function dropmenu() {
   const nav = document.querySelector('#mymenu');
   nav.classList.add('show');
   nav.classList.remove('menu');
-  const mob_header = document.querySelector('#mobheader');
-  mob_header.classList.toggle('close');
+  const mobHeader = document.querySelector('#mobheader');
+  mobHeader.classList.toggle('close');
 }
 
 function closemenu() {
   const nav = document.querySelector('#mymenu');
   nav.classList.remove('show');
   nav.classList.add('menu');
-  const mob_header = document.querySelector('#mobheader');
-  mob_header.classList.remove('close');
-  mob_header.classList.add('mobile-header');
+  const mobHeader = document.querySelector('#mobheader');
+  mobHeader.classList.remove('close');
+  mobHeader.classList.add('mobile-header');
 }
 
 dropmenu();
