1489a7a9bf92689ef771fd15a98250a448621a04
Hans Zizold <hzizold@hotmail.com>
2022-05-11 17:20:20 -0400

    Window Popup Desktop added
    Window Popup Desktop added: 1st version

--- script.js
+++ script.js
@@ -28,88 +28,69 @@ const projects = [
   }
 ]
 // console.log(projects[0].desc);
+const mainContainer = document.querySelector('.main-container');
 const sectionWorks = document.querySelector('.works');
 const popwindow = document.createElement('div');
 const popup = `
-<div class="popup-header">
+  <div class="popup-header">
     <h3 class="multi2">${projects[0].title}</h3>
-    <button class="closebutton">&times;</button>
-</div>
-<ul class="popup-langs">
+    <button onclick="popupclose()" class="closebutton">&times;</button>
+  </div>
+  <ul class="popup-langs">
     <li class="popupli">${projects[0].tech[0]}</li>
     <li class="popupli">${projects[0].tech[1]}</li>
     <li class="popupli">${projects[0].tech[2]}</li>
-</ul>
-<div class="popup-body">
+  </ul>
+  <div class=popup-mainbody>
     <img src="${projects[0].image}" alt="Snapshoot Portfolio">
-    <p>${projects[0].desc}</p>
-</div>
-<div class="popup-buttons">
-    <button type="button" class="popup-live">${projects[0].demo}</button>
-    <button type="button" class="popup-source">${projects[0].src}</button>
-</div>
-`
+    <div class="popup-body">
+      <p>${projects[0].desc}</p>
+      <div class="popup-buttons">
+        <button type="button" class="popup-live">${projects[0].demo}</button>
+        <button type="button" class="popup-source">${projects[0].src}</button>
+      </div>
+    </div>
+  </div>`
 // popup window class to show content: popup-container
 popwindow.innerHTML = popup;
 popwindow.classList.add('close');
-document.body.prepend(popwindow);
-// sectionWorks.prepend(popwindow);
+
+// document.body.prepend(popwindow);
+
+sectionWorks.prepend(popwindow);
+
 // sectionWorks.appendChild(popwindow);
 
 
 function popupwindow() {
   popwindow.id = 'popup-container';
+
   // const thirdc = document.querySelector('#third-container');
   // thirdc.classList.remove('third-container');
   // thirdc.classList.add('close');
-  const mainc = document.querySelector('#main-container');
-  mainc.classList.remove('main-container');
-  mainc.classList.add('close');
+
+  // const mainc = document.querySelector('#main-container');
+  // mainc.classList.remove('main-container');
+  // mainc.classList.add('close');
+
   popwindow.classList.remove('close');
   popwindow.classList.add('popup-container');
-  // $('#popup-container').focus();
+
+  // mainContainer.classList.add('popup-opacity');
+}
+
+function popupclose() {
+  const mainc = document.querySelector('#main-container');
+  mainc.classList.remove('close');
+  mainc.classList.add('main-container');
+
+  popwindow.classList.remove('popup-container');
+  popwindow.classList.add('close');
+
+  // mainContainer.classList.remove('popup-opacity');
 }
 
 
 
-
-
-
-
-  // function popupwindow() {
-  //   let project_popup = {
-  //     pname: '',
-  //     pdescription: '',
-  //     pimage: '',
-  //     ptechno: '',
-  //     plive: '',
-  //     psource: '' 
-  //   };
-
-  // select the ul menu element
-  // const project = document.querySelector('#project');
-
-  // let pdiv = document.createElement('div');
-  // pdiv.id = 'popupid';
-  // pdiv.className = 'popupcl'
-  // pdiv.innerHTML = '<p>Multi Post Stories</p>';
-  // document.body.appendChild(pdiv);
-
-  // let pul = document.createElement('ul');
-  // pul.id = 'pulid'
-  // pdiv.appendChild(pul);
-
-  // let pli = document.createElement('li');
-  // pli.textContent = 'html';
-  // pulid.appendChild(pli);
-
-  // let pli = document.createElement('li');
-  // pli.textContent = 'Bootstrap';
-  // pulid.appendChild(pli);
-
-  // let pli = document.createElement('li');
-  // pli.textContent = 'Ruby on rails';
-  // pulid.appendChild(pli);
-  
 
 
